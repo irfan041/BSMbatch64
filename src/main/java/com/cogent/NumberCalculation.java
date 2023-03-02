216c732202776d88c898b2381f4a47d8d68b8e8e
irfan041 <er.irfan041@gmail.com>
2023-03-02 10:44:48 -0500

    uploading project

---
+++ src/main/java/com/cogent/NumberCalculation.java
@@ -0,0 +1,32 @@
+package com.cogent;
+
+public class NumberCalculation {
+	
+	public int add(int x, int y) {
+		return x+y;
+	}
+
+	public int subtract(int x, int y) {
+		return x-y;
+	}
+
+	public int multiply(int x, int y) {
+		return x*y;
+	}
+
+	public int divide(int x, int y) {
+		return x/y;
+
+	}
+
+}
+
+//junit---Library of java which will used for unit testing
+//what is the need of testing, functionality --Input and output
+//TDD- Test driven development--  test case--Red face, green face, refactring face
+
+
+
+
+
+
